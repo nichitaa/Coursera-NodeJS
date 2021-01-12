126c0924de1181f714f00780fec19b77f8d068c0
nikitaal <nichitastrix@gmail.com>
2021-01-12 21:16:43 +0200

    node-http server handling GET requests

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,55 @@
 const http = require("http");
 
+const fs = require("fs");
+const path = require("path");
+
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-	console.log(req.headers);
+	console.log("Request for " + req.url + " by method " + req.method);
 
-	res.statusCode = 200;
-	res.setHeader("Content-type", "text/html");
-	res.end("<html><body><h1>Hello World</h1></body></html>");
+	if (req.method === "GET") {
+		var fileUrl;
+		if (req.url === "/") fileUrl = "/index.html";
+		else fileUrl = req.url;
+
+		var filePath = path.resolve("./public" + fileUrl);
+		const fileExt = path.extname(filePath);
+		// if the file extension is .html
+		if (fileExt === ".html") {
+			fs.exists(filePath, (exists) => {
+				// file does not exist
+				if (!exists) {
+					res.statusCode = 404;
+					res.setHeader("Content-type", "text/html");
+					res.end(
+						"<html><body><h1>ERROR 404: not found </h1></body></html>"
+					);
+					return;
+				} else {
+					res.statusCode = 200;
+					res.setHeader("Content-type", "text/html");
+					fs.createReadStream(filePath).pipe(res);
+				}
+			});
+		} else {
+			res.statusCode = 404;
+			res.setHeader("Content-type", "text/html");
+			res.end(
+				"<html><body><h1>ERROR 404: not an html file </h1></body></html>"
+			);
+			return;
+		}
+		// if the request.method is not GET
+	} else {
+		res.statusCode = 404;
+		res.setHeader("Content-type", "text/html");
+		res.end(
+			"<html><body><h1>ERROR 404: method not suported </h1></body></html>"
+		);
+		return;
+	}
 });
 
 server.listen(port, hostname, () => {
