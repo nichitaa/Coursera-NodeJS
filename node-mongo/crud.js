78f803ce2424d2edc1d048d6760cba6398510ea5
nikitaal <nichitastrix@gmail.com>
2021-01-13 14:21:58 +0200

    mongo crud operations with .then handling promisses (much better)

--- node-mongo/crud.js
+++ node-mongo/crud.js
@@ -1,50 +1,22 @@
 const assert = require("assert");
-const { EPROTONOSUPPORT } = require("constants");
 
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
 	const coll = db.collection(collection); // get the collection
-	coll.insert(document, (err, result) => {
-		assert.equal(err, null); // if error is null
-		console.log(
-			"INSERTED " +
-				result.result.n +
-				"documents into collection " +
-				collection +
-				"\n"
-		);
-		callback(result); // trigger callback with result object as parameter
-	});
+	return coll.insert(document); // return the promise
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
 	const coll = db.collection(collection); // get the collection
-	coll.find({}).toArray((err, docs) => {
-		assert.equal(err, null); // check if the error null
-
-		callback(docs); // pass the find docs to the callback function
-	});
+	return coll.find({}).toArray(); // return the promise
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
 	const coll = db.collection(collection); // get the collection
-	coll.deleteOne(document, (err, result) => {
-		assert.equal(err, null);
-		console.log("REMOVED DOCUMENT: ", document);
-		callback(result);
-	});
+	return coll.deleteOne(document); // return the promise
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
 	const coll = db.collection(collection); // get the collection
-
 	// update document
-	// 1param - document to be updated
-	// 2param - object with the fields to be updated, (e.g entire update object)
-	// 3param - null
-	// 4parm - callback
-	coll.updateOne(document, { $set: update }, null, (err, result) => {
-		assert.equal(err, null);
-		console.log("DOCUMENT UPDATED WITH: ", update);
-		callback(result);
-	});
+	return coll.updateOne(document, { $set: update }, null); // return the promise
 };
--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,58 +5,46 @@ const crud = require("./crud"); // node module with crud operations
 const url = "mongodb://localhost:27017/";
 const dbname = "confusion";
 
-MongoClient.connect(url, (err, client) => {
-	assert.equal(err, null); // check if error is null
+MongoClient.connect(url)
+	.then((client) => {
+		// assert.equal(err, null); // check if error is null
+		console.log("Connected to the server");
 
-	console.log("Connected to the server");
+		const db = client.db(dbname);
 
-	const db = client.db(dbname);
-
-	crud.insertDocument(
-		db,
-		{
-			name: "Pizza",
-			description: "description",
-		},
-		"dishes",
-		(result) => {
-			console.log("Insert Doc: \n", result.ops); // nr of operations
-
-			// get all documents in dishes collection
-			crud.findDocuments(db, "dishes", (docs) => {
+		crud.insertDocument(
+			db,
+			{
+				name: "dish name",
+				description: "dish description",
+			},
+			"dishes"
+		)
+			.then((result) => {
+				console.log("Inserted Document: ", result.ops);
+				return crud.findDocuments(db, "dishes");
+			})
+			.then((docs) => {
 				console.log("Found Documents: \n", docs);
-
-				// update document
-				crud.updateDocument(
+				return crud.updateDocument(
 					db,
-					{
-						name: "Pizza", // old object
-					},
-					{
-						description: "Updated Description for Pizza", // updated fields
-					},
-					"dishes", // collection
-					(result) => {
-						console.log(("Update docment: \n", result.result)); // result.result -> object updated
-
-						// find documents
-						crud.findDocuments(db, "dishes", (docs) => {
-							console.log(
-								"Found Documents after update: \n",
-								docs
-							);
-
-							// clean db
-							db.dropCollection("dishes", (result) => {
-								console.log("droped Collection: ", result);
-
-								// close connection to db
-								client.close();
-							});
-						});
-					}
+					{ name: "dish name" },
+					{ description: "updated description" },
+					"dishes"
 				);
-			});
-		}
-	);
-});
+			})
+			.then((result) => {
+				console.log(("Update document: \n", result.result));
+				return crud.findDocuments(db, "dishes");
+			})
+			.then((docs) => {
+				console.log("Found Documents after update: \n", docs);
+				return db.dropCollection("dishes");
+			})
+			.then((result) => {
+				console.log("Droped Collection: ", result);
+				client.close();
+			})
+			.catch((err) => console.log(err));
+	})
+	.catch((err) => console.log(err));
